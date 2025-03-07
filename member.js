df3961f7176fbaa8bea920f2c9931d30d578332f
sudhanshu raj <40685426+sudhanshu-raj@users.noreply.github.com>
2025-03-07 10:42:21 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,23 @@
+function skillsMember() {
+    return {
+        title: 'Skills',
+        skills: [
+            {
+                name: 'JavaScript',
+                level: '90%'
+            },
+            {
+                name: 'Node.js',
+                level: '85%'
+            },
+            {
+                name: 'React',
+                level: '80%'
+            },
+            {
+                name: 'Vue.js',
+                level: '75%'
+            }
+        ]
+    }
+}
